484a865d4ce38e90b5cde030048603da7028e1a4
Michael Glenn <dmichaelglenn@gmail.com>
2016-09-06 13:43:58 -0700

    first commit - incomplete code

---
+++ app.js
@@ -0,0 +1,13 @@
+var app = angular.module("nameBadge", []);
+
+app.controller("MainController", function($scope){
+    $scope.firstName = "";
+    $scope.lastName = "";
+    $scope.emailAddress = "";
+    $scope.placeOfBirth = "";
+    $scope.favFood = "";
+    $scope.userBio = "";
+
+    }
+
+});
